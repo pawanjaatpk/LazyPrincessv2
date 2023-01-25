41bc9ec6e05970645073974aed6361f812498b16
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-01-26 00:34:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/pawanjaatpk/LazyPrincessV2bot /LazyPrincessV2bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincessV2bot
+  git clone $UPSTREAM_REPO /LazyPrincessV2
 fi
-cd /LazyPrincessV2bot
+cd /LazyPrincessV2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
